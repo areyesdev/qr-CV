85a6799423d47cc85aaecb3b5007d86f1b51d874
Andres Reyes 🐱‍💻 <ar97yf@gmail.com>
2021-06-02 12:57:04 -0500

    [FIX] await qrcode

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
-const fs = require('fs');
-const qrcode = require('qrcode');
+const fs = require("fs");
+const qrcode = require("qrcode");
 
-const url = "https://drive.google.com/file/d/123UCAkgJNd6WDet4NI-8yt5nYUn_oksf/view?usp=sharing";
+const url =
+  "https://drive.google.com/file/d/123UCAkgJNd6WDet4NI-8yt5nYUn_oksf/view?usp=sharing";
 
-const run = async() => {
-    const QR = await.toDataURL(url)
-    const htmlContent = `
+const run = async () => {
+  const QR = await qrcode.toDataURL(url);
+  const htmlContent = `
         <div style="display: flex; justify-content: center; align-items: center;">
         <h2>Cv Andres Reyes</h2>
         <img src="${QR}">
         </div>
     `;
-    fs.writeFileSync('./index.html', htmlContent)
-}
+  fs.writeFileSync("./index.html", htmlContent);
+};
 
-run()
+run();
